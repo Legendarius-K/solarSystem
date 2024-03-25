a62d54321ac676e822f096cd362675cea3e6f5b6
Darius K <darius.kaya@gmail.com>
2024-03-25 09:34:54 +0100

    add framer motion

--- src/components/Hamburger/index.jsx
+++ src/components/Hamburger/index.jsx
@@ -1,26 +1,28 @@
-import { useState } from 'react';
-import styles from './Hamburger.module.css'
+import { useState, useEffect } from 'react';
+import styles from './Hamburger.module.css';
 
-const Hamburger = ({changePage, closeHamburger, isOpen, xHamburger}) => {
+const Hamburger = ({ changePage, closeHamburger, isOpen, xHamburger }) => {
+    const [secondLevel, setSecondLevel] = useState(false);
 
-    const [secondLevel, setSecondLevel] = useState(false)
+    useEffect(() => {
+        if (!isOpen) {
+            setSecondLevel(false);
+        }
+    }, [isOpen]);
 
     const handleClick = (pageView) => {
-        changePage(pageView)
-        closeHamburger()
-        setSecondLevel(false)
-    }
+        changePage(pageView);
+        closeHamburger();
+        setSecondLevel(false);
+    };
 
     const handleClickHamburger = () => {
-        xHamburger(false)
-    }
+        xHamburger(false);
+    };
 
     const handleClickSecondLevel = () => {
-        setSecondLevel(!secondLevel)
-    }
-
-    
-    
+        setSecondLevel(!secondLevel);
+    };
 
     return (
         <div className={`${styles.hamburger} ${isOpen ? styles.open : ''}`}>
@@ -29,24 +31,26 @@ const Hamburger = ({changePage, closeHamburger, isOpen, xHamburger}) => {
                 <li onClick={() => handleClick(null)}>Home</li>
                 <li onClick={() => handleClick("solarSystem")}>Our Solar System</li>
                 <li onClick={() => handleClick("allPlanets")}>All Planets</li>
-                <li onClick={handleClickSecondLevel} >Planets &#x21a6;
-                    <div className={`${styles.secondLevel} ${secondLevel ? styles.secondLevelOpen : ""}`}>
-                        <ul>
-                            <li onClick={() => handleClick("mercury")}>Mercury</li>
-                            <li onClick={() => handleClick("venus")}>Venus</li>
-                            <li onClick={() => handleClick("earth")}>Earth</li>
-                            <li onClick={() => handleClick("mars")}>Mars</li>
-                            <li onClick={() => handleClick("jupiter")}>Jupiter</li>
-                            <li onClick={() => handleClick("saturn")}>Saturn</li>
-                            <li onClick={() => handleClick("uranus")}>Uranus</li>
-                            <li onClick={() => handleClick("neptune")}>Neptune</li>
-                            <li onClick={() => handleClick("pluto")}>Pluto</li>
-                        </ul>
-                    </div>
+                <li onClick={handleClickSecondLevel}>Planets {secondLevel ? '\u21a5' : '\u21a6'}
+                    {secondLevel && (
+                        <div className={`${styles.secondLevel} ${styles.secondLevelOpen}`}>
+                            <ul>
+                                <li onClick={() => handleClick("mercury")}>Mercury</li>
+                                <li onClick={() => handleClick("venus")}>Venus</li>
+                                <li onClick={() => handleClick("earth")}>Earth</li>
+                                <li onClick={() => handleClick("mars")}>Mars</li>
+                                <li onClick={() => handleClick("jupiter")}>Jupiter</li>
+                                <li onClick={() => handleClick("saturn")}>Saturn</li>
+                                <li onClick={() => handleClick("uranus")}>Uranus</li>
+                                <li onClick={() => handleClick("neptune")}>Neptune</li>
+                                <li onClick={() => handleClick("pluto")}>Pluto</li>
+                            </ul>
+                        </div>
+                    )}
                 </li>
             </ul>
         </div>
-    )
+    );
 };
 
-export default Hamburger
+export default Hamburger;
